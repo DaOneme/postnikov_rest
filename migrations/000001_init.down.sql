248c62e17157ca6b1190820b8da8d95e8bc24ca1
DaOneme <hellofkmail@gmail.com>
2026-03-24 01:32:38 +0300

    infra setup

---
+++ migrations/000001_init.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE restaurant_app.orders;
+DROP TABLE restaurant_app.restaurant_menu;
+DROP TABLE restaurant_app.restaurants;
+DROP TABLE restaurant_app.companies;
+DROP SCHEMA restaurant_app;
